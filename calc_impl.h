c18efbb6b30eff3998430f1f0cdcc7a8696e2258
Stanislav Šidla <100280164+StanislavSidla@users.noreply.github.com>
2024-04-22 15:21:11 +0200

    files added

---
+++ calc_impl.h
@@ -0,0 +1,41 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2024 a.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#ifndef INCLUDED_CUSTOMMODULE_CALC_IMPL_H
+#define INCLUDED_CUSTOMMODULE_CALC_IMPL_H
+
+#include <gnuradio/customModule/calc.h>
+
+namespace gr {
+namespace customModule {
+
+class calc_impl : public calc
+{
+private:
+    int errors;
+    int total;
+    bool processing = false;
+    bool restart = false;
+
+public:
+    calc_impl(bool processing = false, bool restart = false);
+    ~calc_impl();
+   
+    void set_processing(bool new_processing); // Setter for processing
+    void set_restart(bool new_restart); // Setter for restart
+
+   
+    // Where all the action really happens
+    int work(int noutput_items,
+             gr_vector_const_void_star& input_items,
+             gr_vector_void_star& output_items);
+};
+
+} // namespace customModule
+} // namespace gr
+
+#endif /* INCLUDED_CUSTOMMODULE_CALC_IMPL_H */
